8a2eb11909c06458105e47d22d920bf626dc8191
annu2583 <annuverma8234@gmail.com>
2024-07-25 16:15:56 +0530

    project

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import Header from './components/header-footer/header';
 import './snippets/button.css';
-import 
+import './styles/header.css';
 function App() {
   return (
-   <Header/>
+ <>
+ <Header/>
+ </>
   );
 }
 
--- src/components/header-footer/header.jsx
+++ src/components/header-footer/header.jsx
@@ -3,10 +3,16 @@ import React from 'react'
 function Header() {
   return (
   <>
-  class
+ <div className='notification_bar'>
+      <p className='notify'>Get 25% OFF on your first order.Order Now</p>
+ </div>
+ <div className='container'>
+  <img src='./images/logo.png'/>
+ </div>
+ 
   
   </>
   )
-}
+} 
 
-export default Header
+export default Header;
