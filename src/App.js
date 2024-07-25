27777d1d7add1c3c52acbe670edb40c75613c4ae
pinkysahu11 <pinky361511@gmail.com>
2024-07-25 16:37:32 +0530

    remove ecommerce folder

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@
 import './App.css';
 import Header from './components/header-footer/header';
 import './snippets/button.css';
-import 
+import Footer from './components/header-footer/footer';
+import './styles/footer.css';
+
 function App() {
-  return (
-   <Header/>
-  );
+  return(
+
+  <>
+  <Header/>
+  <Footer/>
+  </>
+  )
 }
+
+  
 
 export default App;
