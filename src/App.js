38fbb63a2138940c6c808bccd38f94f0891f8f35
pinkysahu11 <pinky361511@gmail.com>
2024-07-31 14:41:43 +0530

    footer completion

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 
 import './App.css';
-import Header from './components/header-footer/header';
+// import Header from './components/header-footer/header';
 import './snippets/button.css';
-import Footer from './components/header-footer/footer';
+// import Footer from './components/header-footer/footer';
 import './styles/footer.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/home';
 function App() {
   return(
 
   <>
-  <Header/>
-  <Footer/>
+  {/* <Header/>
+  <Footer/> */}
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/Footer' element={<Footer/>}/>
+
+  </Routes>
+  </BrowserRouter>
   </>
   )
 }
--- src/components/header-footer/footer.jsx
+++ src/components/header-footer/footer.jsx
@@ -1,13 +1,59 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
- function Footer() {
+function Footer() {
   return (
-  <>
-    <div className='container'>
-hello i am here
-<img src='\e-commerce\public\html-5.png ' alt='html' />
+    <>
+
+    <Link to={"/Footer"}>
+Footer
+    </Link>
+      <div className='container'>
+
+        <div className='footer'>
+          <div className='title'>
+            <p className='heading_section '>Join Our Newsletter</p>
+            <p className='font_14px'>We love to surprise our subscribers with occasional gifts.</p>
+          </div>
+
+          <div className='input-group'>
+            <input className='font_section  email' type="email" placeholder='Your email address' />
+            <button className='Subscribe-buttton'>Subscribe</button>
+          </div>
+        </div>
       </div>
-  </>
+
+
+      <div className='container'>
+        <div className='footer-container'>
+          <div className='colum_1'>
+            <div className='logo-footer-heading'>
+              <img src='footer-logo.svg' alt='footer_logo' />
+              <p className='heading_section'>ecommerce</p>
+
+            </div>
+            <div className='font_14px'>
+              <p>DevCut is a YouTube channel for </p>
+              <p>practical project-based learning.</p>
+            </div>
+            <div className='logo-section'>
+              <img src='github.svg' />
+              <img src='insta.svg' />
+              <img src='youtube.svg' />
+            </div>
+          </div>
+
+          <div className='column_2'>
+            <ul className='font_section'>SUPPORT </ul>
+            <a><li>FAQ</li></a>
+            <a><li>Terms of use</li></a>
+            <a><li>Privacy Policy</li></a>
+          </div>
+
+          
+        </div>
+      </div>
+    </>
 
 
   )
