6cd6108c5d6dad30e267d3e64f49eae2ea7154a5
annu2583 <annuverma8234@gmail.com>
2024-08-08 15:38:39 +0530

    async

--- src/components/home/bestSeller.jsx
+++ src/components/home/bestSeller.jsx
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+
 function BestSeller() {
+
+  const [productsList, setProductsList] = useState([])
+
+  useEffect(() => {
+    getAllProducts()
+  },[])
+
+  const getAllProducts = async () => {
+
+    await fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(json => setProductsList(json))
+
+  }
+
   return (
     <>
-    <div className='container'>
-    <div className='best_seller section'>
-      <div className='seller_title'>
-      <h4 className='seller_heading'>Shop Now</h4>
-      <h3 className='heading_section'>Best Selling</h3>
+      <div className='container'>
+        <div className='best_seller section'>
+          <div className='seller_title'>
+            <h4 className='seller_heading'>Shop Now</h4>
+            <h3 className='heading_section'>Best Selling</h3>
+          </div>
+          <div className='products_section'>
+            {
+              productsList?.map((item, index) => {
+                return (
+                  <>
+                  <div className='clothing_section'>
+                  <p>{item?.title}</p>
+                    <p>{item?.price}</p>
+                    <img className='card_img' src={item?.image} />
+                  </div>
+            
+
+                  </>
+                )
+
+              })
+            }
+            <div>
+
+            </div>
+
+          </div>
+        </div>
       </div>
 
-    </div>
-    </div>
-    
     </>
   )
 }
